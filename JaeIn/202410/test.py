a98410a96fdae09ad1f8f55ea6d5d5fc7c40ac7f
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-10-25 18:31:06 +0900

    [20241024] BAJ / 실버1 / 숨바꼭질 / 이재인

---
+++ JaeIn/202410/test.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+n , m = map(int , input().split())
+time = 0
+MAX = 100001
+visited = [0] * MAX
+
+
+def bfs(n):
+    queue = deque()
+    queue.append((n , 0))
+    visited[n] = True
+    
+    while queue:
+        num , time = queue.popleft()
+        if num == m:
+            return time
+        for i in [num * 2 , num - 1 , num + 1]:
+           if  0 <= i <= MAX - 1 and not visited[i]:
+                queue.append((i , time + 1))
+                visited[i] = True
+
+result = bfs(n)
+print(result)
+
+    
